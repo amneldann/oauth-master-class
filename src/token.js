717317c6508a0eba7dda5f8770f5632b2868cfcb
amneldann <133861699+amneldann@users.noreply.github.com>
2023-05-17 15:07:07 +0300

    Update token.js

--- src/token.js
+++ src/token.js
@@ -1,6 +1,4 @@
 window.onload = () => {
-  YaSendSuggestToken(
-   'https://oauth-master-class-pi.vercel.app/', {
-      flag: true
-   }
+  YaSendSuggestToken('https://oauth-master-class-pi.vercel.app/'); 
+  {flag: true}
 };

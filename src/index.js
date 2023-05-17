4e022bca884e576e0ef2dc6cab5da2bfde8f98b9
amneldann <133861699+amneldann@users.noreply.github.com>
2023-05-17 14:07:57 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const authorize = ({
 
 // Делаем запрос за инфой о пользователе.
 const fetchYandexData = (token) =>
-  fetch(`https://login.yandex.ru/info?format=json&oauth_token=${token}`).then(
+  fetch(`https://login.yandex.ru/info?format=json&oauth_token=${y0_AgAAAAAs4afUAAnqZgAAAADjTu2LcqMjBfvfRpWDSrgPxPuFc2z1QSk}`).then(
     (res) => res.json()
   );
 

99fc37f82e2814655873798c1a4b434011de8ef8
amneldann <133861699+amneldann@users.noreply.github.com>
2023-05-17 13:38:22 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,7 +17,22 @@ const fetchYandexData = (token) =>
 
 window.onload = () => {
   document.getElementById("suggest").onclick = () => {
-    // TODO suggest
+    // YaAuthSuggest.init({
+         client_id: '--9d1483502b454150a548804fb96747fb--', 
+         response_type: 'token',
+         redirect_uri: '--https://oauth-master-class-pi.vercel.app/token.html--'
+      },
+      '--https://oauth-master-class-pi.vercel.app--'
+   )
+   .then(({ handler }) => handler())
+      .then(async (data) => {
+        const result = await fetchYandexData(data.access_token);
+
+        authorize(result);
+
+        console.log(result, data);
+      })
+      .catch((error) => console.log("Что-то пошло не так: ", error));
   };
   document.getElementById("button").onclick = () => {
     // TODO button
